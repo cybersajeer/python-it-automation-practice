a77afd2d48d7a1461cfec08e662af43e87a6b4a4
Sajeer <abdhulsajeerp@gmail.com>
2026-06-07 18:34:30 +0300

    Add first Python practice script

---
+++ hello_it_automation.py
@@ -0,0 +1,9 @@
+# My first Python practice file for IT automation
+
+name = "Abdhul Sajeer"
+role = "IT Systems Engineer"
+
+print("Hello, GitHub!")
+print("My name is", name)
+print("I am learning Python for IT automation and cybersecurity.")
+print("Current role:", role)
